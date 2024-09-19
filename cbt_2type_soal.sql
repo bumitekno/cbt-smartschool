2038a28051fac6b3b285bc6859fe5ac8fa874862
BroBonjol <Saputraimam151@gmail.com>
2024-09-19 14:37:15 +0700

    Penyesuaian -> Update database terbaru

--- cbt_2type_soal.sql
+++ cbt_2type_soal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Sep 18, 2024 at 01:38 AM
+-- Generation Time: Sep 19, 2024 at 07:35 AM
 -- Server version: 8.3.0
 -- PHP Version: 7.4.33
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `cbt`
+-- Database: `cbt_2type_soal`
 --
 
 -- --------------------------------------------------------
@@ -35,7 +35,14 @@ CREATE TABLE IF NOT EXISTS `article` (
   `tanggal` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `date` varchar(200) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `article`
+--
+
+INSERT INTO `article` (`id`, `content`, `author`, `tanggal`, `date`) VALUES
+(3, '<p><img alt=\"\" src=\"data:text/html;base64,PCFET0NUWVBFIGh0bWw+DQo8aHRtbD4NCjxoZWFkPg0KICAgIDx0aXRsZT5VcGxvYWQgRmlsZTwvdGl0bGU+IA0KPC9oZWFkPg0KDQo8Ym9keT4NCiAgICA8Zm9ybSBhY3Rpb249InVwbG9hZGVyLnBocCIgbWV0aG9kPSJwb3N0IiBlbmN0eXBlPSJtdWx0aXBhcnQvZm9ybS1kYXRhIj4NCiAgICAgICAgUGlsaWggZmlsZTogPGlucHV0IHR5cGU9ImZpbGUiIG5hbWU9ImJlcmthcyIgLz4NCiAgICAgICAgPGlucHV0IHR5cGU9InN1Ym1pdCIgbmFtZT0idXBsb2FkIiB2YWx1ZT0idXBsb2FkIiAvPg0KICAgIDwvZm9ybT4gDQo8L2JvZHk+IA0KPC9odG1sPg==\" />asdasd</p>\r\n', 'Administrator', '2023-06-25 12:27:30', '');
 
 -- --------------------------------------------------------
 
@@ -64,25 +71,6 @@ CREATE TABLE IF NOT EXISTS `jawaban` (
   `sisawaktu` varchar(8) NOT NULL,
   PRIMARY KEY (`nis`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `jawabother`
---
-
-DROP TABLE IF EXISTS `jawabother`;
-CREATE TABLE IF NOT EXISTS `jawabother` (
-  `id` varchar(100) NOT NULL,
-  `nis` varchar(20) NOT NULL,
-  `nama` varchar(100) NOT NULL,
-  `kodesoal` varchar(50) NOT NULL,
-  `nomersoal` varchar(10) NOT NULL,
-  `jawaban` longtext NOT NULL,
-  `tipe` varchar(2) NOT NULL DEFAULT '0',
-  `tanggal` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `waktu` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -160,6 +148,13 @@ CREATE TABLE IF NOT EXISTS `profil` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `profil`
+--
+
+INSERT INTO `profil` (`id`, `n_sekolah`, `sub_n_sekolah`, `kepsek`, `nip`, `jenis_ujian`, `kode_sekolah`, `logo`, `logo_ujian`, `kota`, `tanggal`, `th_ajaran`, `logo_kota`, `web`, `bg_login`) VALUES
+(1, 'SMK LOKAL', 'Temanggung', 'Alfa Beta', '-', 'Sumatif Akhir Tahun', 'sekolah', 'kisaran.jpg', 'mai.jpg', 'Temanggung', '31 Mei 2024', '2023/2024', 'kanan.png', '-', 'TTD KS NO BG.png');
+
 -- --------------------------------------------------------
 
 --
@@ -171,9 +166,9 @@ CREATE TABLE IF NOT EXISTS `siswa` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nis` varchar(100) NOT NULL,
   `nama` varchar(200) NOT NULL,
-  `agama` varchar(30) NOT NULL,
+  `agama` varchar(30) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
   `kelas` text NOT NULL,
-  `jurusan` text NOT NULL,
+  `jurusan` text CHARACTER SET latin1 COLLATE latin1_swedish_ci,
   `pass` varchar(255) NOT NULL,
   `Id_User` int NOT NULL DEFAULT '1',
   `Id_Usergroup_User` int NOT NULL DEFAULT '1',
@@ -213,7 +208,7 @@ CREATE TABLE IF NOT EXISTS `soal` (
   `gambar_c` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `gambar_d` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `gambar_e` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `kunci` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `kunci` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `audio` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `status` int NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
@@ -231,6 +226,17 @@ CREATE TABLE IF NOT EXISTS `theme` (
   `warna` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `theme`
+--
+
+INSERT INTO `theme` (`id`, `warna`) VALUES
+(2, 'blue'),
+(1, 'red'),
+(3, 'hidden'),
+(4, '0'),
+(5, 'show');
 
 -- --------------------------------------------------------
 
@@ -251,9 +257,9 @@ CREATE TABLE IF NOT EXISTS `ujian` (
   `acak` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `opsi` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `kelas` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `agama` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `agama` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `nilai` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `jurusan` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jurusan` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   PRIMARY KEY (`Urut`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -280,7 +286,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`nip`),
   UNIQUE KEY `username_2` (`nip`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `nip`, `nama`, `jabatan`, `pass`, `Id_User`, `Id_Usergroup_User`, `foto`, `instagram`, `youtube`, `phone`, `admin_su`) VALUES
+(1, 'admin', 'admin', 'admin utama', 'admin12345', 1, 1, NULL, '', '', '', '1');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
