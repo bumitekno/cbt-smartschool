39edced336a423714be7ea474e5dcf6611f960a8
BroBonjol <Saputraimam151@gmail.com>
2024-11-07 14:33:59 +0700

    Penyesuaian -> ujianstart admin dibuat no autologout dan countdown waktu ujian

--- on-siswa/ujianstart.php
+++ on-siswa/ujianstart.php
@@ -136,19 +136,18 @@ while ($xx = mysqli_fetch_array($qq)) {
 
       function timer() {
         count = count - 1;
-        if (count == -1) {
-          document.getElementById("form1").submit();
-          return;
+        if (count < 0) {
+            clearInterval(counter);
+            document.getElementById("form1").submit();
+            return;
         }
 
         var seconds = count % 60;
-        var minutes = Math.floor(count / 60);
-        var hours = Math.floor(minutes / 60);
-        minutes %= 60;
-        hours %= 60;
+        var minutes = Math.floor(count / 60) % 60;
+        var hours = Math.floor(count / 3600);
 
-        document.getElementById("divwaktu").innerHTML = hours + " jam : " + minutes + " menit"; // watch for spelling
-      }
+        document.getElementById("divwaktu").innerHTML = hours + " jam : " + minutes + " menit : " + seconds + " detik";
+    }
     </script>
   </head>
   <!-------navbar------>
