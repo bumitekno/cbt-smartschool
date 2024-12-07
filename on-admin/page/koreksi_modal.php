76be5242be59cf8187b72e56e44cae117115669d
alfajri23 <feri.alfajri@gmail.com>
2024-12-07 10:13:45 +0700

    Tampilkan data jawaban uraain jawaban kosong tdk disimpan

--- on-admin/page/koreksi_modal.php
+++ on-admin/page/koreksi_modal.php
@@ -28,46 +28,59 @@ input[type="radio"] {
 <input type="hidden" name="nama" id="nama" value="<?php echo $nama; ?>">
 <input type="hidden" name="ks" id="ks" value="<?php echo $kodesoal; ?>">
 <input type="hidden" name="nis" id="nis" value="<?php echo $nis; ?>">
+
 <?php 
 
-$query = mysqli_query($konek, "SELECT * FROM jawaburaian WHERE nis='$nis' AND kodesoal='$kodesoal' ORDER by nomersoal ASC");
+//$query = mysqli_query($konek, "SELECT * FROM jawaburaian WHERE nis='$nis' AND kodesoal='$kodesoal' ORDER by nomersoal ASC");
+
+$query = mysqli_query($konek, "SELECT * FROM soal WHERE kodesoal='$kodesoal' AND status='2' ORDER by nomersoal ASC");
+
 if($query == false){
 	die ("Terjadi Kesalahan : ". mysqli_error($konek));
 }
 
 $i=0;
-while($r = mysqli_fetch_array($query)){
-    
-		$nosol=$r['nomersoal'];
-		$query2 = mysqli_query($konek, "SELECT * FROM soal WHERE kodesoal='$kodesoal' AND nomersoal='$nosol' ORDER by nomersoal ASC");
-		if($query2 == false){
-			die ("Terjadi Kesalahan : ". mysqli_error($konek));
+
+while($or = mysqli_fetch_array($query)){
+	
+	$nosol=$or['nomersoal'];
+
+	//$query2 = mysqli_query($konek, "SELECT * FROM soal WHERE kodesoal='$kodesoal' AND nomersoal='$nosol' ORDER by nomersoal ASC");
+	
+	$query2 = mysqli_query($konek, "SELECT * FROM jawaburaian WHERE nis='$nis' AND kodesoal='$kodesoal' AND nomersoal='$nosol' ORDER by nomersoal ASC");
+	if($query2 == false){
+		die ("Terjadi Kesalahan : ". mysqli_error($konek));
+	}
+
+	// soal
+		if(!$or['gambarsoal']=='')
+		{
+		$gambarsoal = "<br><img src='../gbr/$or[gambarsoal]' alt='Nature' class='responsive' align=center style='max-width:500px; height:auto' ><br>";
 		}
-		while($or = mysqli_fetch_array($query2)){
-			if(!$or['gambarsoal']=='')
-			{
-			$gambarsoal = "<br><img src='../gbr/$or[gambarsoal]' alt='Nature' class='responsive' align=center style='max-width:500px; height:auto' ><br>";
-			}
-			else
-			{
-			$gambarsoal = "";
-			}
-			if(!$or['audio']=='')
-			{
-			$audio = "<br><audio src='../gbr/$or[audio]' controls controlsList='nodownload'></audio>";
-			}
-			else
-			{
-			$audio = "";
-			}
-								
-			$i++; 
-?>				
+		else
+		{
+		$gambarsoal = "";
+		}
+		if(!$or['audio']=='')
+		{
+		$audio = "<br><audio src='../gbr/$or[audio]' controls controlsList='nodownload'></audio>";
+		}
+		else
+		{
+		$audio = "";
+		}
+							
+		
+	// end soal
+
+?>
+
 	<div class="col-xs-12">
+		<!-- Soal -->
 		<div class="attachment">
 			<a href="#" class="name">
 				<br>
-				Soal nomer <?php echo $r['nomersoal']; ?>
+				Soal nomer <?php echo $or['nomersoal']; ?>
 			</a>
 			<p class="filename">
 				<?php echo $or['soal']; ?>
@@ -76,10 +89,17 @@ while($r = mysqli_fetch_array($query)){
 				<?php echo $audio; ?>
 			</p>
 		</div>
+
+	</div>
+		
+<?php
+	// Jawaban siswa
+	while($r = mysqli_fetch_array($query2)){
+		$i++; 
+		
+?>		
+	<div class="col-xs-12 2">		
 		<div class="attachment">
-
-	
-
 			<a class="name">
 				Jawaban :
 			</a>
@@ -95,37 +115,46 @@ while($r = mysqli_fetch_array($query)){
 				</a>
 			<?php endif; ?>
 
-			
+
 			</p>
 		</div>
 	</div>
 
-<div class="container<?php echo $i; ?>" id="tengah">
 
-    <label class="radio-inline">
-      <input type="radio" name="optradio<?php echo $r['nomersoal']; ?>" value="1"<?php echo ($r['nilai']=='1')?'checked="checked"':'' ?>/>&nbsp;1
-    </label>
-    <label class="radio-inline">
-      <input type="radio" name="optradio<?php echo $r['nomersoal']; ?>" value="2"<?php echo ($r['nilai']=='2')?'checked="checked"':'' ?>>&nbsp;2
-    </label>
-    <label class="radio-inline">
-      <input type="radio" name="optradio<?php echo $r['nomersoal']; ?>" value="3"<?php echo ($r['nilai']=='3')?'checked="checked"':'' ?>>&nbsp;3
-    </label>
-	<label class="radio-inline">
-      <input type="radio" name="optradio<?php echo $r['nomersoal']; ?>" value="4"<?php echo ($r['nilai']=='4')?'checked="checked"':'' ?>>&nbsp;4
-    </label>
-	<label class="radio-inline">
-      <input type="radio" name="optradio<?php echo $r['nomersoal']; ?>" value="5"<?php echo ($r['nilai']=='5')?'checked="checked"':'' ?>>&nbsp;5
-    </label>
-  
-</div>
+	
+	<div class="container<?php echo $i; ?>" id="tengah">
 
-<hr class="style-two">	
+		<label class="radio-inline">
+		<input type="radio" name="optradio<?php echo $r['nomersoal']; ?>" value="1"<?php echo ($r['nilai']=='1')?'checked="checked"':'' ?>/>&nbsp;1
+		</label>
+		<label class="radio-inline">
+		<input type="radio" name="optradio<?php echo $r['nomersoal']; ?>" value="2"<?php echo ($r['nilai']=='2')?'checked="checked"':'' ?>>&nbsp;2
+		</label>
+		<label class="radio-inline">
+		<input type="radio" name="optradio<?php echo $r['nomersoal']; ?>" value="3"<?php echo ($r['nilai']=='3')?'checked="checked"':'' ?>>&nbsp;3
+		</label>
+		<label class="radio-inline">
+		<input type="radio" name="optradio<?php echo $r['nomersoal']; ?>" value="4"<?php echo ($r['nilai']=='4')?'checked="checked"':'' ?>>&nbsp;4
+		</label>
+		<label class="radio-inline">
+		<input type="radio" name="optradio<?php echo $r['nomersoal']; ?>" value="5"<?php echo ($r['nilai']=='5')?'checked="checked"':'' ?>>&nbsp;5
+		</label>
+	
+	</div>
 
-<?php }} ?>		
-&nbsp;&nbsp;&nbsp;<input type="submit" name="submit" value="submit"/>
-  </form>						
+	<hr class="style-two">	
+
+<?php 
+
+	}
+} 
+
+?>	
+
+	&nbsp;&nbsp;&nbsp;
+	<input type="submit" name="submit" value="submit"/>
+  </form>		
+		
 							<div class="modal-footer">
 							</div>
 					</div>
-				</div>
